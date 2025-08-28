6bc8bb7de7d36f7f8a22c8fb720deb2315514d9a
ashiruma <ashirumaabala@gmail.com>
2025-08-28 19:35:43 +0300

    Update urls.py

--- core_api/urls.py
+++ core_api/urls.py
@@ -2,13 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-   
+    path("admin/", admin.site.urls),
+
     # API endpoints
     path("api/users/", include("users.urls")),
     path("api/films/", include("films.urls")),
     path("api/payments/", include("payments.urls")),
     path("api/analytics/", include("analytics.urls")),
-    path("admin/", admin.site.urls),
-    path("users/", include("users.urls")),
-    path("payments/", include("payments.urls")),
 ]
