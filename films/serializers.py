0097d716e8648d04c06b55ee02913daff0b79b4e
ashiruma <ashirumaabala@gmail.com>
2025-08-28 10:27:22 +0300

    Update serializers.py

--- films/serializers.py
+++ films/serializers.py
@@ -7,12 +7,22 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = ["id", "name", "slug"]
 
 class FilmSerializer(serializers.ModelSerializer):
-    category = CategorySerializer(read_only=True)
+    category = CategorySerializer(read_only=True)  # show category details
 
     class Meta:
         model = Film
         fields = [
-            "id", "title", "slug", "description", "status",
-            "price", "poster", "trailer_url", "video_file",
-            "category", "created_at", "updated_at"
+            "id",
+            "title",
+            "slug",
+            "description",
+            "poster",
+            "trailer_url",
+            "video_file",
+            "price",
+            "rental_period_days",
+            "status",
+            "category",
+            "created_at",
+            "updated_at",
         ]
