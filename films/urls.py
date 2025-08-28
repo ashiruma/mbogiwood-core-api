c4ebdb2e3931bb22898aab0303c8ff05607ff47f
ashiruma <ashirumaabala@gmail.com>
2025-08-28 10:23:15 +0300

    Update urls.py

--- films/urls.py
+++ films/urls.py
@@ -1,6 +1,14 @@
 # films/urls.py
 from django.urls import path
 from . import views
+from rest_framework.routers import DefaultRouter
+from .views import FilmViewSet, CategoryViewSet
+
+router = DefaultRouter()
+router.register(r'films', FilmViewSet, basename="films")
+router.register(r'categories', CategoryViewSet, basename="categories")
+
+urlpatterns = router.urls
 
 app_name = "films"
 
