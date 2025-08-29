75d66cd67b8dab29de28a13a9ca7384cd454de68
ashiruma <ashirumaabala@gmail.com>
2025-08-29 19:12:16 +0300

    Update serializers.py

--- films/serializers.py
+++ films/serializers.py
@@ -10,13 +10,14 @@ class CategorySerializer(serializers.ModelSerializer):
 
 
 class FilmSerializer(serializers.ModelSerializer):
-    # This will now use the serializer above to show category details
+    # This will use the serializer above to show category details
     category = CategorySerializer(read_only=True)
     
-    # We will rename trailer_url to match the frontend's expectation
-    trailer_link = serializers.URLField(source='trailer_url')
-    # We will rename poster to poster_image to match the frontend
-    poster_image = serializers.ImageField(source='poster', read_only=True)
+    # We rename `trailer_url` from the model to `trailer_link` to match the frontend
+    trailer_link = serializers.URLField(source='trailer_url', read_only=True)
+    
+    # We rename `poster` from the model to `poster_image` and build the full URL
+    poster_image = serializers.SerializerMethodField()
 
     class Meta:
         model = Film
@@ -26,8 +27,16 @@ class FilmSerializer(serializers.ModelSerializer):
             "title",
             "slug",
             "description",
-            "release_date", # Make sure to add this field to your view
+            "release_date",
             "poster_image",
             "trailer_link",
             "category",
         ]
+
+    def get_poster_image(self, obj):
+        request = self.context.get('request')
+        if obj.poster and hasattr(obj.poster, 'url'):
+            if request is not None:
+                return request.build_absolute_uri(obj.poster.url)
+            return obj.poster.url
+        return None
