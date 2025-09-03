0878982a52dc5d13b1e15e5108eb82d3096f4e3d
Chrispine Abala <ashiruma@gmail.com>
2025-09-03 09:47:47 +0300

    feat: Finalize and unify all app configurations and URL routing

--- core_api/settings.py
+++ core_api/settings.py
@@ -32,6 +32,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 CORS_ALLOWED_ORIGINS = [o for o in os.getenv("CORS_ALLOWED_ORIGINS", "").split(",") if o]
